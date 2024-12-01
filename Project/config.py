f02fddbc08ad434845f21a03fdc2512af3cd6ead
rusan <rusandrkozak@gmail.com>
2024-12-01 22:07:19 +0300

    Project step save 2

---
+++ Project/config.py
@@ -0,0 +1,7 @@
+IS_NOTEBOOK = True
+PATH = None
+
+if IS_NOTEBOOK:
+    PATH = "D:\BigFiles\Projects Programming\Яндекс Специализация осень 2024 (Big Data)\dataset_telemetry.csv"
+else:
+    PATH = "D:\Курсы(образование)\Яндекс Специализации\BigData\Project\dataset_telemetry.csv"
